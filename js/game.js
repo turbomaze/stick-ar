a5069c08e86ba5fe8acc3a001af3db9d9467951a
Anthony Liu <anthonyscottliu@gmail.com>
2017-07-30 03:11:39 -0700

    Move a paddle

--- js/game.js
+++ js/game.js
@@ -5,12 +5,57 @@ class Game {
     this.canvas = canvas;
     this.ctx = ctx;
     this.state = {
+      // ball
       x: 0.5,
       y: 0.5,
       radius: 0.05,
       speed: 0.05,
       angle: Math.random(2 * Math.PI),
+
+      // paddle 1
+      paddle1x: 0.1,
+      paddle1y: 0.5,
+      paddle1vel: 0,
+
+      // paddle 2
+      paddle2x: 0.9,
+      paddle2y: 0.5,
+      paddle2vel: 0,
     };
+  }
+
+  start() {
+    const self = this;
+    this.canvas.addEventListener('mousedown', (e) => {
+      const clickData = self.getClickSegments(e);
+      if (clickData.segment.left) {
+        self.updateState({paddle1vel: -0.07});
+      } else if (clickData.segment.right) {
+        self.updateState({paddle1vel: 0.07});
+      }
+    });
+
+    this.canvas.addEventListener('mouseup', (e) => {
+      const clickData = self.getClickSegments(e);
+      self.updateState({paddle1vel: 0});
+    });
+  }
+
+  getClickSegments(e) {
+    const rect = this.canvas.getBoundingClientRect();
+    const pos = { x: e.clientX - rect.left, y: e.clientY - rect.top };
+    const segment = {};
+    if (pos.x < (rect.right - rect.left) / 2) {
+      segment.left = true;
+    } else {
+      segment.right = true;
+    }
+    if (pos.y < (rect.bottom - rect.top) / 2) {
+      segment.top = true;
+    } else {
+      segment.bottom = true;
+    }
+    return {pos, segment}
   }
 
   updateState(newState) {
@@ -31,9 +76,21 @@ class Game {
     const ballSize = this.state.radius * (corners[3][0] - corners[0][0]);
     const p = this.project([this.state.x, this.state.y], corners);
     this.drawPoint(p[0], p[1], ballSize, 'rgb(90, 80, 88)');
+
+
+    // draw the paddles
+    this.drawFatSegment(
+      corners,
+      this.state.paddle1x, this.state.paddle1y, 0.05, 0.15, 'rgb(60, 60, 60)'
+    );
+    this.drawFatSegment(
+      corners,
+      this.state.paddle2x, this.state.paddle2y, 0.05, 0.15, 'rgb(60, 60, 60)'
+    );
   }
 
   advanceState() {
+    // move the balls
     const radius = this.state.radius;
     const newX = this.state.x + this.state.speed * Math.cos(this.state.angle);
     const newY = this.state.y + this.state.speed * Math.sin(this.state.angle);
@@ -54,6 +111,14 @@ class Game {
         angle: (2 * Math.PI - this.state.angle) % (2 * Math.PI)
       });
     }
+
+    // move the paddles
+    const newPaddle1Y = this.state.paddle1y + this.state.paddle1vel;
+    const newPaddle2Y = this.state.paddle2y + this.state.paddle2vel;
+    this.updateState({
+      paddle1y: Math.max(Math.min(newPaddle1Y, 1), 0),
+      paddle2y: Math.max(Math.min(newPaddle2Y, 1), 0)
+    });
   }
 
   project(point, corners) {
@@ -84,6 +149,34 @@ class Game {
     this.ctx.fillStyle = color;
     this.ctx.beginPath();
     this.ctx.arc(x, y, r, 0, 2 * Math.PI, true);
+    this.ctx.closePath();
+    this.ctx.fill();
+  }
+
+  // x, y are the center
+  drawFatSegment(corners, mathX, mathY, width, height, color) {
+    const topLeft = this.project([
+      mathX - width/2, mathY - height/2
+    ], corners);
+    const topRight = this.project([
+      mathX + width/2, mathY - height/2
+    ], corners);
+    const bottomLeft = this.project([
+      mathX - width/2, mathY + height/2
+    ], corners);
+    const bottomRight = this.project([
+      mathX + width/2, mathY + height/2
+    ], corners);
+    this.drawPolygon([topLeft, topRight, bottomRight, bottomLeft], color)
+  }
+
+  drawPolygon(points, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.beginPath();
+    this.ctx.moveTo.apply(this.ctx, points[0]);
+    for (let i = 1; i < points.length; i++) {
+      this.ctx.lineTo.apply(this.ctx, points[i]);
+    }
     this.ctx.closePath();
     this.ctx.fill();
   }
--- js/stick.js
+++ js/stick.js
@@ -1,80 +1,3 @@
-class OpticalFlow {
-  constructor(options) {
-    this.curr_img_pyr = new jsfeat.pyramid_t(3);
-    this.prev_img_pyr = new jsfeat.pyramid_t(3);
-    this.curr_img_pyr.allocate(
-      options.width,
-      options.height,
-      jsfeat.U8_t|jsfeat.C1_t
-    );
-    this.prev_img_pyr.allocate(
-      options.width,
-      options.height,
-      jsfeat.U8_t|jsfeat.C1_t
-    );
-    this.point_count = 0;
-    this.point_status = new Uint8Array(100);
-    this.prev_xy = new Float32Array(100*2);
-    this.curr_xy = new Float32Array(100*2);
-    this.options = options;
-  }
-
-  update(imageData) {
-    const _pt_xy = this.prev_xy;
-    this.prev_xy = this.curr_xy;
-    this.curr_xy = _pt_xy;
-    const _pyr = this.prev_img_pyr;
-    this.prev_img_pyr = this.curr_img_pyr;
-    this.curr_img_pyr = _pyr;
-
-    jsfeat.imgproc.grayscale(
-      imageData.data,
-      imageData.width,
-      imageData.height,
-      this.curr_img_pyr.data[0]
-    );
-    this.curr_img_pyr.build(this.curr_img_pyr.data[0], true);
-
-    jsfeat.optical_flow_lk.track(
-      this.prev_img_pyr,
-      this.curr_img_pyr,
-      this.prev_xy,
-      this.curr_xy,
-      this.point_count,
-      this.options.win_size|0,
-      this.options.max_iterations|0,
-      this.point_status,
-      this.options.epsilon,
-      this.options.min_eigen
-    );
-    this.prune_oflow_points();
-  }
-
-  prune_oflow_points() {
-    const n = this.point_count;
-    for (var i = 0, j = 0; i < n; ++i) {
-      if (this.point_status[i] == 1) {
-        if (j < i) {
-          this.curr_xy[j<<1] = this.curr_xy[i<<1];
-          this.curr_xy[(j<<1)+1] = this.curr_xy[(i<<1)+1];
-        }
-        ++j;
-      }
-    }
-    this.point_count = j;
-  }
-
-  addPoint(x, y) {
-    this.curr_xy[this.point_count<<1] = x;
-    this.curr_xy[(this.point_count<<1)+1] = y;
-    this.point_count++;
-  }
-
-  clearPoints() {
-    this.point_count = 0;
-  }
-}
-
 class Stick {
   static init() {
     const stick = new Stick(
@@ -107,6 +30,7 @@ class Stick {
           StickARUtils.forceFullScreen();
         }
         stick.isFullScreen = true;
+        stick.game.start();
       }
     });
     if (document.location.hash !== '#desktop') {
@@ -129,15 +53,6 @@ class Stick {
     this.region = false;
     this.totalTime = 0;
     this.totalFrames = 0;
-    this.flow = new OpticalFlow({
-      width: this.width,
-      height: this.height,
-      sampleRate: this.sampleRate,
-      win_size: 30,
-      max_iterations: 20,
-      epsilon: 0.05,
-      min_eigen: 0.005
-    });
     this.game = new Game(
       this.width, this.height,
       this.gameCanvas, this.gameCtx
@@ -202,44 +117,24 @@ class Stick {
       0, 0,
       this.width*this.sampleRate, this.height*this.sampleRate
     );
-    const haveEnoughFlowPoints = this.flow.point_count === 4;
-    let didOflow = false;
-    if (this.region && this.region.score > 0.95 && haveEnoughFlowPoints) {
-      this.flow.update(computeData);
-      if (this.flow.point_count === 4) {
-        this.region.corners = [];
-        for (let i = 0; i < this.flow.point_count; i++) {
-          this.region.corners.push([
-            this.flow.curr_xy[(i << 1)], this.flow.curr_xy[(i << 1) + 1]
-          ]);
-        }
-        this.region.corners = StickARUtils.sortCorners(this.region.corners);
-        didOflow = true;
-      }
-    }
 
-    if (!didOflow) {
-      const candidateRegion = this.getBestSquare(computeData);
-      const maxStale = 2;
-      if (candidateRegion) {
-        this.region = candidateRegion;
-      } else if (this.region && (this.region.stale || 0) < maxStale) {
-         this.region.stale = (this.region.stale || 0) + 1;
-      } else {
-        this.region = false;
-      }
-      this.flow.clearPoints();
-      if (this.region) {
-        this.region.corners.forEach(this.flow.addPoint.bind(this.flow));
-      }
+    const candidateRegion = this.getBestSquare(computeData);
+    const maxStale = 2;
+    if (candidateRegion) {
+      this.region = candidateRegion;
+    } else if (this.region && (this.region.stale || 0) < maxStale) {
+       this.region.stale = (this.region.stale || 0) + 1;
+    } else {
+      this.region = false;
     }
+    
     this.renderGame();
   }
 
   getBestSquare(imageData) {
     const self = this;
     const data = imageData.data;
-    const threshold = 600;
+    const threshold = 530;
     const newData = new ImageData(imageData.width, imageData.height);
     for (let i = 0; i < data.length; i += 4) {
       const bright = data[i] + data[i + 1] + data[i + 2];
